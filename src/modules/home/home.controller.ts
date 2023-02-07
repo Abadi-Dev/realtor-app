afa61bd72c4cd34418ffae295fd98e1c88331e03
Abdulelah Alamri <Abadi_dev@outlook.com>
2023-02-08 01:21:08 +0300

    query params for filtering

--- src/modules/home/home.controller.ts
+++ src/modules/home/home.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, Query } from '@nestjs/common';
 import { HomeService } from './home.service';
 import { HomeResponseDto } from './dto/home.dto';
+import { PropertyType } from '@prisma/client';
 
 @Controller('home')
 export class HomeController {
   constructor(private readonly homeService: HomeService) {}
 
   @Get()
-  getHomes(): Promise<HomeResponseDto[]> {
+  getHomes(
+    @Query('city') city?: string,
+    @Query('maxPrice') maxPrice?: string,
+    @Query('minPrice') minPrice?: string,
+    @Query('propertType') propertType?: PropertyType,
+  ): Promise<HomeResponseDto[]> {
     return this.homeService.getHome();
   }
 
